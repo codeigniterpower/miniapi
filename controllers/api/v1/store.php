9778fdd5b7cc4065dfe0995589f130c1f5dd71b4
mckaygerhard <mckaygerhard@gmail.com>
2024-04-14 18:26:47 -0400

    example with one parameters in url and three arguments
    * provide default project of geany

--- controllers/api/v1/store.php
+++ controllers/api/v1/store.php
@@ -9,13 +9,27 @@ class api_v1_store_controller extends controller {
 
     public function execute($post = null) {
 
-        $parameters = array(0=>"store");
+        $parameters = array(0=>"lists");
+        $arguments = array('id'=>-1);
 
         $parameters = $this->router->parameters;
-        
         foreach($parameters as $key => $value) {
-            $$value = $value;
+            $$key = $value;
         }
+        $arguments = array_merge($arguments, $_GET);
+        if ($_SERVER["REQUEST_METHOD"] == "GET") {
+            foreach($_GET as $keyp => $valuep) {
+                $$keyp = $valuep;
+            }
+        }
+        $arguments = array_merge($arguments, $_POST);
+        if ($_SERVER["REQUEST_METHOD"] == "POST") {
+            foreach($_POST as $keyp => $valuep) {
+                $$keyp = $valuep;
+            }
+        }
+
+        $this->model->processdata($parameters, $arguments);
 
         $this->model->show();
 
--- models/api/v1/store.php
+++ models/api/v1/store.php
@@ -8,15 +8,61 @@
 
 class api_v1_store_model extends model {
 
-  public function notFound() {
-    $this->borrow('notFoundApi')->show();
-  }
+    private $errormessage = 'error, cannot store values of data, error on parameters or arguments or error in data storage';
+    private $urlarguments = null;
+    private $savedatatodb = false;
+    private $savedatafull = false;
+    private $codehttpisok = 404;
 
-  public function show() {
-    $errormessage = 'function not found or parameters are incorrect';
-    $httpcode = 404;
-    $variables = array('message'=>$errormessage,'httpcode'=>$httpcode);
-    $this->renderOutput($variables,true);
-  }
+    public function notFound() {
+        $this->borrow('notFoundApi')->show();
+    }
+
+    public function processdata($parameter, $urlarguments) {
+
+        //api/vi/lists/<asd>?PAL=UPV&ced1=nnnn&ced2=nnn
+
+        $varpal = 'NUL,(not received)';
+        $varis0 = array_key_exists('PAL', $urlarguments);
+        if($varis0) $varpal = $urlarguments['PAL'];
+
+        $varcd1 = 'NUL,(not received)';
+        $varis1 = array_key_exists('ced1', $urlarguments);
+        if($varis1) $varcd1 = $urlarguments['ced1'];
+
+        $varcd2 = 'NUL,(not received)';
+        $varis2 = array_key_exists('ced2', $urlarguments);
+        if($varis2) $varcd2 = $urlarguments['ced2'];
+
+        $validator   = new validator($urlarguments);
+        $validations = array(
+            'PAL' => array(
+                'type'      => 'string',
+                "required"  => true
+            ),
+            'ced1' => array(
+                'type'      => 'integer',
+                "required"  => true
+            ),
+            'ced2' => array(
+                'type'      => 'integer',
+                "required"  => false
+            ),
+        );
+        $check = $validator->execute($validations);
+
+        $this->sucess = $check[0];
+        if($this->sucess) 
+            $this->codehttpisok = 200;
+        $this->errormessage = ''.$check[1].' received parameteres are '.$varpal.' '.$varcd1.' '.$varcd2;
+    }
+
+    public function show($parameters = null) {
+      $errormessage = $this->errormessage;
+      $httpcode = $this->codehttpisok;
+      $sucess = $this->sucess;
+      $variables = array('sucess'=>$sucess,'message'=>$errormessage,'httpcode'=>$httpcode,'data'=>$parameters);
+      $this->renderOutput($variables,true);
+    }
 
 }
